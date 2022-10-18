7d98e37c83c9783ce47b2fcaa22c3f7769a8e3bf
Yanguk <99335782+Yanguk@users.noreply.github.com>
2022-10-18 11:57:03 +0900

    feat: 모달창 구현 (#6)
    * feat: 모달창 구현
    
    - eslint airbnb 규칙에 따라 proptype을 검사하기위해 package.json에prop-types가 추가되었습니다.
    - react potal 로 구현하여서 html안에 div가 추가되었습니다.
    - theme 컬러에 모달 배경색을 추가하였습니다.
    * refacotr: Modal 컴포넌트 사용하기쉽게 useModal 훅 구현 완료

---
+++ src/components/common/Modal/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { AiOutlineClose } from "react-icons/ai";
+
+import Portal from "./Portal";
+import theme from "../../../config/constants/theme";
+
+export default function Modal({ children, isOpen, setIsOpen }) {
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleOuterClick = (e) => {
+    e.stopPropagation();
+
+    if (e.currentTarget === e.target) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <Portal>
+      <Container onClick={handleOuterClick}>
+        <Wrapper>
+          <CloseButton onClick={() => setIsOpen(false)}>
+            <CloseIcon />
+          </CloseButton>
+          {children}
+        </Wrapper>
+      </Container>
+    </Portal>
+  );
+}
+
+Modal.propTypes = {
+  children: PropTypes.element.isRequired,
+  setIsOpen: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+};
+
+const Container = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: ${theme.modalBackGround};
+  cursor: pointer;
+
+  animation-name: fadeIn;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-out;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  width: 60vw;
+  height: 50vh;
+  max-width: 500px;
+  max-height: 280px;
+  padding: 32px 20px;
+  border: none;
+  border-radius: 10px;
+  background-color: ${theme.gray};
+  -webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000,
+    5px 5px 15px 5px rgba(153, 153, 153, 0);
+  box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000,
+    5px 5px 15px 5px rgba(153, 153, 153, 0);
+  transform: translate(-50%, -50%);
+  cursor: auto;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  overflow: visible;
+  top: 10px;
+  left: 10px;
+  padding: 0;
+  border: none;
+  border-radius: 0;
+  background: inherit;
+  box-shadow: none;
+  cursor: pointer;
+`;
+
+const CloseIcon = styled(AiOutlineClose)`
+  font-size: 20px;
+`;
---
+++ src/hooks/useModal.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Modal from "../components/common/Modal";
+
+export default function useModal(initialState = false) {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const toggleModal = () => setIsOpen(!isOpen);
+
+  const customModal = ({ children }) => {
+    return (
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+        {children}
+      </Modal>
+    );
+  };
+
+  return [customModal, toggleModal, isOpen];
+}
