696ab37bdd7ef7fbe0ee2d2fa98a3d59e2660b9d
Choi Song Yi <95201149+ssong-yi@users.noreply.github.com>
2022-10-23 01:25:48 +0900

    [2022.10.22] feat: error page 구현 (#10)
    * feat: error page 구현
    
    404 에러 페이지, 500 에러 페이지 구현.
    보여줄 나머지 에러는 토스트메시지로 전달할 예정.
    
    * refactor: pr template 수정
    
    PR Template 수정 및 version 업데이트
    
    Co-authored-by: csy <csl8804@gmail.com>

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,5 +1,85 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../config/constants/theme";
 
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <ErrorWrapper>
+        <ErrorBox>
+          <ErrorCode>500</ErrorCode>
+          <ErrorImage />
+        </ErrorBox>
+        <ErrorMessage>Internal Server Error</ErrorMessage>
+      </ErrorWrapper>
+      <HomeButton onClick={() => navigate("/")}>
+        <Home>홈으로</Home>
+      </HomeButton>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.white};
+  height: 100%;
+`;
+
+const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorCode = styled.span`
+  font-size: 130px;
+  font-weight: 800;
+  color: rgba(45, 101, 173, 1);
+`;
+
+const ErrorImage = styled.img.attrs({
+  src: "https://s3-alpha-sig.figma.com/img/8910/7ee8/44f3635380822bad3c731d99ac54eae1?Expires=1667174400&Signature=MJ2mHAjUZJvrOTkURbXJ9ysIN8Q0vI61T6pRkrUbXwk2XKYQa2s2neiW2E1Gs76r2c1RUro-wgVca~JvhVUl3xwmgU-lEDot3Ni-iJXfcRyZTxDeYH-m3s8z4zZdjxARr9UchZdSglLG1O79T0TFtOuJqqVzND~ortHZvn1kb5i0OeJXSr07YKVNuwJLSx8GK63QrfOZGU8TY1rMe2~StRL5f2ZgYmE1ZBumOdrJZG-Q-36ZbGYfStq6tjt2kxTAkAuwaCLq-Ahgvjy1SAyMZrCxYTN3YQLoMSdTCUD16hqgrtjGf-AX97dDizchwcsSRKI~xXVS3A1SrFM6MN8U1w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  alt: "error image",
+})`
+  width: 580px;
+  height: 450px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 40px;
+`;
+
+const HomeButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10vh;
+  padding: 10px;
+  border: 1.5px solid black;
+  border-radius: 5px;
+  background-color: ${theme.white};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.gray};
+    color: ${theme.black};
+    transition: all 0.1s ease-in-out;
+  }
+`;
+
+const Home = styled.span`
+  font-size: 20px;
+`;
--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,5 +1,70 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../config/constants/theme";
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <ErrorWrapper>
+        <ErrorImage />
+        <ErrorMessage>존재하지 않는 페이지에 대한 요청입니다.</ErrorMessage>
+      </ErrorWrapper>
+      <HomeButton onClick={() => navigate("/")}>
+        <Home>홈으로</Home>
+      </HomeButton>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.white};
+  height: 100%;
+`;
+
+const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorImage = styled.img.attrs({
+  src: "https://s3-alpha-sig.figma.com/img/e558/c9ca/3bc0c09c04ff82e3696a07b73b057b86?Expires=1667174400&Signature=dQpoc-s~3lw7B405biW9c~YxJMjUqCgpbi8DRd8oeoIWjM~qZaS-wVWqRsWKooTSgCVItfIHiqiQSaND23lhmCgYGEuES3VPT0Nf-LyqnI2U0gdE87JSoRSQLrDCWAiJ~~VZxZVvWBm~XwxWjxCfwu4I4J5Ufck-JiRwKhGSPJgbAqwS2HUpo07FJ9eU7AIou9vwWCVilLzTdwDdt7XiHsNqHSPm3eO~NWIZ00v22Y3PJZtZ9XB4yT6ZmCIlYOJ4li3XpadhKiBYwoz~B8~QbeAS-r2CPUvvT6PpVmug2zl3xQ7mmeyTTg~VeDXpcTqYZwkEmFgIE3usAhzYHXqVzg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  alt: "not found image",
+})`
+  width: 715px;
+  height: 440px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 20px;
+`;
+
+const HomeButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10vh;
+  padding: 10px;
+  border: 1.5px solid black;
+  border-radius: 5px;
+  background-color: ${theme.white};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.gray};
+    color: ${theme.black};
+    transition: all 0.1s ease-in-out;
+  }
+`;
+
+const Home = styled.span`
+  font-size: 20px;
+`;
